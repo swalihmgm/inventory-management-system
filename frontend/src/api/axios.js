de60344865570ac09fc251bdec8077e8a3eb1560
swalihmgm <sualihmgm21@gmail.com>
2026-09-06 12:43:12 +0530

    Connect frontend to production backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/",
+    baseURL: "https://inventory-management-system-oen7.onrender.com",
 });
 
 api.interceptors.request.use(
